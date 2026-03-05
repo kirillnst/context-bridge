c14adc677f96f9f7b963d5aee6f21ea5a94f4a4a
Kirill Nesterov <kirillnst@gmail.com>
2026-03-05 18:43:25 +0300

    remove unused command and related console log from extension

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "context-bridge" is now active!');
+	//console.log('Congratulations, your extension "context-bridge" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('context-bridge.helloWorld', () => {
+	//const disposable = vscode.commands.registerCommand('context-bridge.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Context Bridge!');
-	});
+	//	vscode.window.showInformationMessage('Hello World from Context Bridge!');
+	//});
 
-	context.subscriptions.push(disposable);
+	//context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
